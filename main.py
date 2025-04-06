ac00a26bdbcd3127cae3f76b5cfae6588f55445c
liubq <304145520@qq.com>
2025-04-06 11:12:51 +0800

    Changes

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import random
-def get_max(lst):
-    x = lst[0]
-    for i in range(1,len(lst)):
-        if lst[i] > x:
-           x=lst[i]
-    return x
-lst = [random.randint(1,100) for item in range(10)]
-print(lst)
-max = get_max(lst)
-print(max)
 
 
 
